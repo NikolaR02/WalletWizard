cc3e684848eb34bf9d09e491f6c29c3eea3d10af
Nicola <nik.nik.raich@gmail.com>
2023-11-05 17:53:15 +0100

    Esqueleto de la Pantalla Inicial

--- app/src/main/java/com/example/walletwizard/MainActivity.kt
+++ app/src/main/java/com/example/walletwizard/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.walletwizard
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import com.example.walletwizard.databinding.InicioBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: InicioBinding
+    private var saldo: Double = 500.98
+    private var activos = arrayOf("BBVA ...3695\n${saldo}€","Caixa ...4534\n${saldo}€","Efectivo\n${saldo}€")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = InicioBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvSaldoTotal.text= getString(R.string.saldo_total, saldo)
+        binding.lvSaldos.adapter = ArrayAdapter(this,R.layout.lv_activos_item,activos)
+
+
     }
+
+
 }
